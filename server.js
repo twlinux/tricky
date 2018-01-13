4d2f1c9b76d23e0b1f74f25c351d3430c15cdbe3
Jennings Zhang <jenni_zh@protonmail.com>
2018-01-13 15:18:16 -0500

    Cloud storage

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const correct = {
 };
 const loginPage = '/public/login.html';
 
+const fs = require('fs');
+
 const ip = require('ip');
 const colors = require('colors');
 
@@ -29,7 +31,7 @@ app.post('/user/page/:file', urlencodedParser, function (req, res) {
     if (!req.body) return res.sendStatus(400);
     if (req.body.username === correct.user && req.body.pass === correct.pass) {
         res.cookie(cookieKey, true);
-        res.sendFile(req.params.file, {root: `${__dirname}/user/page/`});
+        res.sendFile(req.params.file, { root: `${__dirname}/user/page/` });
         output(req, colors.cyan('gave cookie'), res.statusCode);
     }
     else
@@ -37,16 +39,38 @@ app.post('/user/page/:file', urlencodedParser, function (req, res) {
 });
 
 //check cookie then serves content
-app.get('/user/page/:file', function (req, res) {
+app.get('/user/:folder/:file', function (req, res) {
+
+    let message;
+    if (!req.cookies.login) {
+        res.status(403).end();
+        message = colors.red('denied');
+    }
+    else {
+        res.sendFile(req.params.file, { root: `${__dirname}/user/${req.params.folder}/` });
+        if (req.params.folder === `data`)
+            message = colors.cyan('data');
+        else if (req.params.file.endsWith('.html'))
+            message = colors.cyan('allowed');
+    }
+    if (message)
+        output(req, message, res.statusCode);
+});
+
+//list of files in /user/data in JSON format
+app.get('/user/dataFiles.json', function (req, res) {
 
     if (!req.cookies.login) {
         res.status(403).end();
         return;
     }
-    
-    res.sendFile(req.params.file, {root: `${__dirname}/user/page/`});
-    if (req.params.file.endsWith('.html'))
-        output(req, colors.cyan('allowed'), res.statusCode);
+
+    fs.readdir(__dirname + '/user/data/', function (err, items) {
+        if (err)
+            res.status(500).end();
+        else
+            res.json(items);
+    });
 });
 
 app.all('/user/logout*', function (req, res) {
@@ -72,16 +96,16 @@ function output(req, info, statusCode) {
 
     let request;
     switch (req.method) {
-    case 'GET':
-        request = `${colors.green(req.method)} ${info}`;
-        break;
-    case 'POST':
-        request = `${colors.blue(req.method)} ${colors.bold(info.magenta)}`;
-        break;
-    case false:
-        request = 'invalid';
-    default:
-        request = colors.red(req.method);
+        case 'GET':
+            request = `${colors.green(req.method)} ${info}`;
+            break;
+        case 'POST':
+            request = `${colors.blue(req.method)} ${colors.bold(info.magenta)}`;
+            break;
+        case false:
+            request = 'invalid';
+        default:
+            request = colors.red(req.method);
     }
     let date = new Date();
     console.log(colors.dim(`[  ${date.getHours()}:${date.getMinutes()} ${date.getSeconds()} ]`)
--- user/page/user.js
+++ user/page/user.js
@@ -1,4 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    document.getElementById('name-placeholder').innerText = 'Hillary'
+    document.getElementById('name-placeholder').innerText = 'user hilarious';
+
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.open('GET', '/user/dataFiles.json', true);
+    xhr.setRequestHeader('Cache-Control', 'no-cache');
+    var app = document.getElementById('app');
+    xhr.onload = function () {
+        if (this.status != 200) {
+            app.innerText = 'XMLHTTPRequest status ' + this.status;
+            return;
+        }
+        app.appendChild(createMatCollection(this.response));
+    }
+    xhr.onerror = function() {
+        app.innerText = 'XMLHTTPRequest.onerror';
+    }
+    xhr.send();
 });
+
+/**
+ * 
+ * @param {string[]} list 
+ */
+function createMatCollection(list) {
+    var collection = document.createElement('div');
+    collection.className = 'collection';
+    list.forEach(fileName => {
+        let item = document.createElement('a');
+        item.className = `collection-item`;
+        item.href = '/user/data/' + fileName;
+        item.innerText = fileName;
+        item.setAttribute('download', '');
+        collection.appendChild(item);
+    });
+    return collection;
+}
